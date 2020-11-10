6233514509864f9bfa645b511a1345bf16df9f7c
Giovanni Palleschi <gpalleschi@gmail.com>
2020-11-10 17:32:38 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# pyExpresso
+
+<div style=”border: 1px solid #000″>
+	<img src="img/pyExpresso.png" alt="pyAsn1Tk"
+		title="pyAsn1Tk" width="200" height="200" align="center"/>
+</div>		
+
+## Description
+
+A Visual Tool to check regular expression written in Python using Kivy GUI framework, it permits test expressions, save expression on file and load expression from a file.
+
+<div style=”border: 1px solid #000″ align="center">
+	<img src="https://drive.google.com/uc?export=view&id=1DZ30W3Lqle-5-FM8xwqAmUmu7llhHvJA" alt="pyAsn1Tk"
+		title="pyAsn1Tk"  height="400"/>
+</div>		
+<br>
+
+## Prerequisites
+
+[Python 3.x](https://www.python.org/)  
+[Kivy 1.11.1](https://kivy.org/#home)  
+
+## Built With  
+
+* [Visual Code Editor](https://code.visualstudio.com)  
+
+## Authors
+
+* **Giovanni Palleschi** - [gpalleschi](https://github.com/gpalleschi)  
+
+## License
+
+This project is licensed under the GNU GENERAL PUBLIC LICENSE 3.0 License - see the [LICENSE](LICENSE) file for details
---
+++ pyExpresso.py
@@ -0,0 +1,115 @@
+from kivy.app import App
+from kivy.properties import ObjectProperty
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy_deps import sdl2, glew
+import re
+import os
+
+
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class SaveDialog(FloatLayout):
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+class pyExpressoRE:
+    def reset(objTest):
+        objTest.background_color = [1, 1, 1, 1]
+
+    def execute(pattern, objTest):
+        if len(pattern) > 0:
+            if len(objTest.text) > 0:
+                result = re.match(pattern, objTest.text)
+                if result:
+# Expression is Ok
+                    objTest.background_color = [0, 1, 0, 1]
+                else:
+# Expression is Ko 
+                    objTest.background_color = [1, 0, 0, 1]
+            else:
+                 objTest.background_color = [1, 1, 1, 1]
+
+class PyExpressoForm(BoxLayout):
+    test1 = ObjectProperty(None)
+    test2 = ObjectProperty(None)
+    test3 = ObjectProperty(None)
+    regexp = ObjectProperty(None)
+    def validate_regexp(self):
+        if len(self.regexp.text) > 0:
+            if len(self.test1.text) > 0:
+                self.validate_test1()
+            if len(self.test2.text) > 0:
+                 self.validate_test2()
+            if len(self.test3.text) > 0:
+                self.validate_test3()
+        else:
+            pyExpressoRE.reset(self.test1) 
+            pyExpressoRE.reset(self.test2) 
+            pyExpressoRE.reset(self.test3) 
+    
+    def validate_test1(self):
+        pyExpressoRE.execute(self.regexp.text,self.test1)
+    def validate_test2(self):
+        pyExpressoRE.execute(self.regexp.text,self.test2)
+    def validate_test3(self):
+        pyExpressoRE.execute(self.regexp.text,self.test3)
+
+    def popHelp(self):
+        box = BoxLayout(orientation='vertical')
+        box.add_widget(Image(source='.\img\pyExpresso.png'))
+        box.add_widget(Label(text='           Version : 1.0\nAuthor  : Giovanni Palleschi'))
+        pop = Popup(title='pyExpresso',content=box,
+                    size_hint=(None,None), size=(300, 300))
+        pop.open()
+    loadfile = ObjectProperty(None)
+    savefile = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def show_load(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def show_save(self):
+        content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Save file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def load(self, path, filename):
+        with open(os.path.join(path, filename[0])) as stream:
+            self.regexp.text = stream.read()
+            self.test1.text = ""
+            self.test2.text = ""
+            self.test3.text = ""
+
+        self.dismiss_popup()
+
+    def save(self, path, filename):
+        with open(os.path.join(path, filename), 'w') as stream:
+            stream.write(self.regexp.text)
+
+        self.dismiss_popup()
+
+
+class pyExpresso(App):
+    def build(self):
+        self.icon = '.\img\pyExpresso.png'
+        return PyExpressoForm()
+
+if __name__ == '__main__':
+	pyExpresso().run()
